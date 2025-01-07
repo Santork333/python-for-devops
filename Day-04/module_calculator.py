91ea04292f6052c6e3ed4ff8ac48df52a811837f
Santork333 <santhoshkumarb164@gmail.com>
2025-01-07 13:07:24 +0000

    mm

--- Day-04/module_calculator.py
+++ Day-04/module_calculator.py
@@ -1,8 +1,10 @@
 import calc_function_approach
 
-calc_function_approach
+calc_function_approach()
 
 def multiply():
+    num1 = 10
+    num2 = 15
     multiply = num1 * num2
     print("Multilication of num1 and num2 is", multiply)
 
