eb3e07c157d7ea5170a53005f848f85e0fa7bd47
Santork333 <santhoshkumarb164@gmail.com>
2025-01-07 12:22:24 +0000

    Imported a calculator module

---
+++ Day-04/calc_function_approach.py
@@ -0,0 +1,14 @@
+num1 = 11 # Declaring the num1 as Global variable
+num2 = 6 # Declaring the num1 as Global variable
+
+def addition(): # declaring the addition function
+    addition = num1 + num2
+    print("Addition of num1 and num2 is", addition)
+
+def sub(): # declaring the subtraction function
+    sub = num1 - num2
+    print("Subtraction of num1 and num2 is", sub)
+
+# Invoking the below functions
+addition()
+sub()
