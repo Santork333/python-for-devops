ad04ea3697d22124b476543a347da53f2da01a21
Santork333 <santhoshkumarb164@gmail.com>
2025-01-07 11:40:08 +0000

    added calculator app

---
+++ Day-04/calculator-function.py
@@ -0,0 +1,13 @@
+num1 = 11
+num2 = 6
+
+def addition():
+    addition = num1 + num2
+    print("Addition of num1 and num2 is", addition)
+
+def sub():
+    sub = num1 - num2
+    print("Subtraction of num1 and num2 is", sub)
+
+addition()
+sub()
---
+++ Day-04/calculator.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 6
+
+addition = num1 + num2
+print("Addition of num1 and num2 is", addition)
+
+subtraction = num1 - num2
+print("Subtraction of num1 and num2 is", subtraction)
+
