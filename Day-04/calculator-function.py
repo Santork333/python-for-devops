67bde8591c7b3df02b35862aa2e65526558eb067
Santork333 <santhoshkumarb164@gmail.com>
2025-01-07 11:43:02 +0000

    added the declarative comments

--- Day-04/calculator-function.py
+++ Day-04/calculator-function.py
@@ -1,13 +1,14 @@
-num1 = 11
-num2 = 6
+num1 = 11 # Declaring the num1 as Global variable
+num2 = 6 # Declaring the num1 as Global variable
 
-def addition():
+def addition(): # declaring the addition function
     addition = num1 + num2
     print("Addition of num1 and num2 is", addition)
 
-def sub():
+def sub(): # declaring the subtraction function
     sub = num1 - num2
     print("Subtraction of num1 and num2 is", sub)
 
+# Invoking the below functions
 addition()
 sub()
