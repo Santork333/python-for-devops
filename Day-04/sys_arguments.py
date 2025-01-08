5c60336ea6a8aabf93b3deda15626da83b6a88aa
Santork333 <santhoshkumarb164@gmail.com>
2025-01-08 09:21:56 +0000

    sys arguments

---
+++ Day-04/sys_arguments.py
@@ -0,0 +1,20 @@
+import os
+
+def addition(num1, num2):
+    addition = (num1 + num2)
+    return addition
+
+def multiplication(num1, num2):
+    multiplication = (num1 * num2)
+    return multiplication
+
+num1 = sys.argv[1]
+operation = sys.argv[2]
+num2 = sys.argv[3]
+
+if operation == "addition"
+   print(addition(num1, num2))
+
+if operation == "multiplication"
+   print(multiplication(num1, num2))
+
